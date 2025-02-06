0df5951df6edbb8891246ae69b434e0698eb5d90
NexusGKSoftwares <nexusgksoftwares@gmail.com>
2025-02-06 11:32:21 +0300

    `Added code for removing duplicates and detecting outliers in a DataFrame`

--- week3.py
+++ week3.py
@@ -63,23 +63,88 @@ df.dropna(inplace=True, axis=1) # drop columns with missing values
 
 
 # removing the duplicates
-# 1 drop the duplicate rows
+# 1. drop the duplicate rows
 
 # example
 
 # df.drop_duplicates(inplace=True)
 
 # example 2
-data = {'ID': [1,2,2,3,4,4,5,]}
+# data = {'ID': [1,2,2,3,4,4,5,]}
 
+# df = pd.DataFrame(data)
+
+# duplicates = df[df.duplicated()]
+# print("Duplicate Rows:\n", duplicates)
+
+# removed_duplicates = df[df.duplicated(keep=False)]
+# print("All Duplicates:\n ", duplicates)
+# df_cleaned = df.drop_duplicates()
+
+# print("\n Data after removing duplicates:\n", df_cleaned)
+
+
+#  2. drop duplicates based on specific columns
+
+# df.drop_duplicates(subset=['Column Name'], inplace=True)
+
+
+#  3. Keeping the first and the last occurence
+
+# df.drop_duplicates(keep="first", inplace=True)
+# df.drop_duplicates(keep="last", inplace=True)
+
+
+# DETECTING & Treating Outliers
+
+
+# 1. detecting the outliers
+# box plot
+
+# example
+
+import seaborn as sns 
+import matplotlib.pyplot as plt 
+
+data = {'Salary': [50000,52000,51000,100000,53000,49000]}
 df = pd.DataFrame(data)
+# sns.boxplot(x=df['Salary'])
+# plt.show()
 
-duplicates = df[df.duplicated()]
-print("Duplicate Rows:\n", duplicates)
 
-removed_duplicates = df[df.duplicated(keep=False)]
-print("All Duplicates:\n ", duplicates)
-df_cleaned = df.drop_duplicates()
+# df['Salary'].hist(bins=30)
+# plt.xlabel('Salary')
+# plt.ylabel('frequency')
+# plt.title('Salary distribution')
+# plt.show()
 
-print("\n Data after removing duplicates:\n", df_cleaned)
+# interquartile range (IQR) 
+#  The IQR is the difference between the 75th percentile (Q3) and the 25th percentile (Q1)
+#  The IQR is a measure of the spread of the data, and it can be used
+#  to detect outliers.
 
+# outliers are values below Q1-1.5 * IQR or above Q3+1.5 * IQR
+
+# example
+Q1 = df['Salary'].quantile(0.25)
+Q3 = df['Salary'].quantile(0.75)
+IQR = Q3 - Q1
+
+# define the outliers boundaries
+lower_bound = Q1 - 1.5 * IQR
+upper_bound = Q3 + 1.5 * IQR
+
+# print the outliers boundaries
+# print(f"Lower bound: {lower_bound}")
+# print(f"Upper bound: {upper_bound}")
+
+
+# hint
+# data inside the lower and upper bounds are not outliers [ Q1 - 1.5 X IQR , Q3 + 1.5 X IQR ] -- normal
+# data outside the lower and upper bounds are outliers [ below Q1 - 1.5 X IQR, above Q3 + 1.5 X IQR ] -- abnormal
+
+# 2. treating the outliers
+
+#  - removing the outliers
+df_filtered = df[(df['Salary'] > lower_bound) & (df['Salary'] < upper_bound)]
+print(df_filtered)
