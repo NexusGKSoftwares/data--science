ea22bf0b226b9105ffb1af1fa4b890c22a8de471
NexusGKSoftwares <nexusgksoftwares@gmail.com>
2025-02-06 10:40:38 +0300

    `Added handling of missing values and duplicates in week3.py`

--- week3.py
+++ week3.py
@@ -22,7 +22,7 @@ df.dropna(inplace=True, axis=1) # drop columns with missing values
 # - median imputation: replace missing values with the median of the column(numerical values)
 # df['Age'].fillna(df['Age'].median(), inplace=True)
 # - mode imputation: replace missing values with the most frequent value in the column(categorical values)
-df['Cabin'].fillna(df['Cabin'].mode()[0], inplace=True)
+# df['Cabin'].fillna(df['Cabin'].mode()[0], inplace=True)
 
 
 # - forward fill(use previous values)
@@ -30,6 +30,56 @@ df['Cabin'].fillna(df['Cabin'].mode()[0], inplace=True)
 # # - backward fill(use next values)
 # df['Age'].fillna(method='bfill', inplace=True) # backward fill
 
-# handling duplicates
 
+
+
+# mode
+# data = {'Name': ['Tom', 'nick', 'krish', 'jack', None, 'Nick', None]}
+
+# df = pd.DataFrame(data)
+
+# print("before filling missing values",)
+# print(df)
+# #  filling the missing
+
+# df['Name'].fillna(df['Name'].mode()[0], inplace=True)
+
+# print("\n After filling missing values",)
+# print(df)
+
+# sometimes mode fails due to:
+# incorrect data types
+# empty mode result
+# multiple modes
+# missing values
+
+#  2 Handling duplicates
+
+# duplicate data can distort insights and leads to biased results
+
+# # identify the duplicates
+# print(df.duplicated().sum()) # -- count the duplicate rows
+# print(df[df.duplicated()]) # -- display duplicate rows
+
+
+# removing the duplicates
+# 1 drop the duplicate rows
+
+# example
+
+# df.drop_duplicates(inplace=True)
+
+# example 2
+data = {'ID': [1,2,2,3,4,4,5,]}
+
+df = pd.DataFrame(data)
+
+duplicates = df[df.duplicated()]
+print("Duplicate Rows:\n", duplicates)
+
+removed_duplicates = df[df.duplicated(keep=False)]
+print("All Duplicates:\n ", duplicates)
+df_cleaned = df.drop_duplicates()
+
+print("\n Data after removing duplicates:\n", df_cleaned)
 
